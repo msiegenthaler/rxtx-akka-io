1c266a5e74f6094c03f06b782442b3c6c5090125
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-12-13 13:32:10 +0100

    Events and commands for the operator.

--- src/main/scala/rxtxio/Serial.scala
+++ src/main/scala/rxtxio/Serial.scala
@@ -2,6 +2,7 @@ package rxtxio
 
 import akka.actor._
 import akka.io._
+import akka.util.ByteString
 
 /**
  * Serial port extension based on the rxtx library for the akka IO layer.
@@ -23,20 +24,47 @@ object Serial extends ExtensionId[SerialExt] with ExtensionIdProvider {
 
   // Communication with manager
 
-  /** Message that is sent to the manager actor. */
-  sealed trait ManagerMessage
+  /** Command that may be sent to the manager actor. */
+  sealed trait ManagerCommand extends Command
+  /** Command that may be sent to the operator actor. */
+  sealed trait OperatorCommand extends Command
 
   /** Open a serial port. Response: Opened | CommandFailed */
-  case class Open(port: String, baudRate: Int) extends Command with ManagerMessage
+  case class Open(port: String, baudRate: Int) extends ManagerCommand
 
-  /** Serial port is now open. Communication is handled by the operator. */
-  case class Opened(operator: ActorRef, port: String) extends Event with ManagerMessage
+  /**
+   *  Serial port is now open.
+   *  Communication is handled by the operator actor.
+   *  The sender of the Open message will now receive incoming communication from the
+   *  serial port.
+   */
+  case class Opened(operator: ActorRef, port: String) extends Event
 
   /** List all available serial ports. Response: Ports | CommandFailed */
-  case object ListPorts extends Command with ManagerMessage
+  case object ListPorts extends ManagerCommand
 
   /** Available serial ports. */
-  case class Ports(ports: Vector[String]) extends Event with ManagerMessage
+  case class Ports(ports: Vector[String]) extends Event
+
+  // Communication with Operator
+
+  /** Request that the operator should close the port. Response: Closed */
+  case object Close extends OperatorCommand
+
+  /** The port was closed. Either by request or by an external event (i.e. unplugging) */
+  case object Closed extends Event
+
+  /** Data was received on the serial port. */
+  case class Received(data: ByteString) extends Event
+
+  /** Write data on the serial port. */
+  case class Write(data: ByteString, ack: AckEvent = NoAck) extends OperatorCommand
+
+  /** Ack for a write. */
+  trait AckEvent extends Event
+
+  /** Special ack event (is not sent). */
+  object NoAck extends AckEvent
 }
 
 class SerialExt(system: ExtendedActorSystem) extends akka.io.IO.Extension {
