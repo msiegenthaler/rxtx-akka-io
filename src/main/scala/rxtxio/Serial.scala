5ed330c2a49fab9ae84fb4f1307642ebad575d42
Mario Siegenthaler <msiegenthaler@inventsoft.ch>
2013-12-13 11:44:26 +0100

    Consolidate Serial extension.

--- src/main/scala/rxtxio/Serial.scala
+++ src/main/scala/rxtxio/Serial.scala
@@ -1,8 +1,23 @@
 package rxtxio
 
-import akka.actor.ExtensionKey
+import akka.actor._
+import akka.io._
 
-/** Messages used by the serial IO. */
-object Serial extends ExtensionKey[SerialExt] {
+/** Serial port extension based on the rxtx library for the akka IO layer. */
+object Serial extends ExtensionId[SerialExt] with ExtensionIdProvider {
+  override def lookup = Serial
+  override def createExtension(system: ExtendedActorSystem): SerialExt = new SerialExt(system)
 
+  /** Messages used by the serial IO. */
+  sealed trait Message
+
+  /** Messages that are sent to the serial port. */
+  sealed trait Command extends Message
+
+  /** Messages received from the serial port. */
+  sealed trait Event extends Message
+}
+
+class SerialExt(system: ExtendedActorSystem) extends akka.io.IO.Extension {
+  lazy val manager = system.actorOf(Props(classOf[SerialManager]), name = "IO-SERIAL")
 }
